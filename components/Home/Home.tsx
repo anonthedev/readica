9ede0199d40db01689a69ab131c3e74315cf349d
anonthedev <opss59332@gmail.com>
2024-07-07 17:43:29 +0530

    modified home page

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,39 +1,18 @@
 "use client";
 
-import { useState } from "react";
-import { Input } from "@/components/ui/input";
-import { Button, buttonVariants } from "@/components/ui/button";
-import { parseXml } from "@/utils/xmlParser";
-import { SearchedPaperDetails } from "@/utils/types";
-import axios from "axios";
+import { buttonVariants } from "@/components/ui/button";
 import leftBg from "@/resources/images/left-bg.png";
 import rightBg from "@/resources/images/right-bg.png";
 import { useUser } from "@clerk/nextjs";
 import Link from "next/link";
 
 export default function Home() {
-  const [query, setQuery] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [results, setResults] = useState<SearchedPaperDetails[]>([]);
   const { isSignedIn, isLoaded } = useUser();
 
-  async function getPapers() {
-    setLoading(true);
-    axios
-      .get(`https://export.arxiv.org/api/query?search_query=all:${query}`)
-      .then((resp) => {
-        console.log(resp.data);
-        setResults(parseXml(resp.data));
-        setLoading(false);
-      })
-      .catch((err) => {
-        console.log(err);
-        setLoading(false);
-      });
-  }
-
   return (
-    <main className={`w-screen min-h-[calc(100vh-80px)] flex flex-row items-center justify-between`}>
+    <main
+      className={`w-screen min-h-[calc(100vh-80px)] flex flex-row items-center justify-between`}
+    >
       <img src={leftBg.src} alt="" className="h-full w-fit" />
       <section className="flex flex-col items-center justify-center gap-10 w-full">
         <div className="flex flex-col gap-4 max-w-prose text-center items-center justify-center">
