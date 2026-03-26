7003015ce7d58d0931c2a37c5296917f1201f5aa
anonthedev <opss59332@gmail.com>
2026-03-26 16:04:46 +0530

    new supabase db and everything back to working again

--- client/src/lib/supabase.ts
+++ client/src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://mktpqsgqflwenwhijsjm.supabase.co";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://wjoyqrbhrnncoqpaicpe.supabase.co";
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY!;
 // console.log(supabaseKey)
 
